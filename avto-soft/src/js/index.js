1a6f7a0e348c3ef450315e71e722268f3f72e027
Andrey Karchev <karchev_sait@mail.ru>
2020-08-26 12:45:37 +0300

    #avto-soft. Сделана страница добавления заказа

--- avto-soft/src/js/index.js
+++ avto-soft/src/js/index.js
@@ -19,6 +19,19 @@ $('.dropdown-menu-filter').on('click', function(e) {
     e.stopPropagation();
 });
 
+// Вывод списка выбранных файлов
+function handleFileSelect(evt) {
+    var files = evt.target.files;
+
+    var output = [];
+    for (var i = 0, f; f = files[i]; i++) {
+        output.push('<label class="custom-radio">' + escape(f.name) + '<input type="checkbox" name=""><span class="checkmark checkmark-checkbox" title="Показывать только выбранному исполнителю"></span></label>');
+    }
+    document.getElementById('file-list').innerHTML = output.join('');
+}
+
+document.getElementById('file-input').addEventListener('change', handleFileSelect, false);
+
 // hamburg init
 var forEach = function(t, o, r) {
     if ("[object Object]" === Object.prototype.toString.call(t))
